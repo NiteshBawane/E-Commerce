bda400d3e17745567a0b4c8330b1d00b67eb178c
NiteshBawane <bawane51@gmail.com>
2021-12-02 14:50:45 +0530

    first commit

---
+++ src/main/java/com/TestBases/IndexPageTest.java
@@ -0,0 +1,45 @@
+package com.TestBases;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.PageDistribution.pages.IndexPage;
+import com.aventstack.extentreports.model.Log;
+
+public class IndexPageTest extends TestBase {
+	WebDriver driver;
+	IndexPage ip;
+
+	@BeforeMethod
+	public void setUp() {
+		driver =intialization();
+		ip = new IndexPage(driver);
+	}
+	
+	
+	@AfterMethod()
+	public void tearDown() {
+	driver.quit();
+	}
+	
+	@Test
+	public void verifyLogo(){
+		ip= new IndexPage(driver);
+		boolean result=ip.validateLogo();
+		Assert.assertTrue(result);
+		
+	}
+	
+	@Test
+	public void verifyTitle() {
+		
+		String actTitle=ip.getTitle();
+		Assert.assertEquals(actTitle, "Zen Cart!, The Art of E-commerce");
+		
+	}
+
+	
+}
---
+++ src/main/java/com/TestBases/SearchResultPageTest.java
@@ -0,0 +1,35 @@
+package com.TestBases;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.PageDistribution.pages.IndexPage;
+import com.PageDistribution.pages.SearchResultPage;
+
+
+public class SearchResultPageTest extends TestBase{
+	WebDriver driver;
+	SearchResultPage searchResultPage;
+	IndexPage ip;
+	@BeforeMethod
+	public void setUp() {
+		driver =intialization();
+		ip=new IndexPage(driver);
+		
+	}
+
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+		
+	  @Test 
+	  public void productAvailabilityTest(String productName) {
+		  searchResultPage=ip.searchProduct(productName);
+		  boolean result=searchResultPage.isProductAvailable();
+			Assert.assertTrue(result);
+	  }
+}
